b08b1d56a78a38acc72f6836eb4d6884d365e46e
Stanislas Signoud <signez@stanisoft.net>
2016-11-01 23:08:07 +0100

    Add a simple frontend that lists translations
    As always, those commits includes generated content to make sure
    the frontend interface can be booted without having to use Brunch.

---
+++ src/frontend/app/components/translation-group.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="translation-group">
+    <translation-key :translation-key="translationKey" />
+    <div class="translation" v-for="translation in translations"
+      :t-id="translation.id">
+      <div class="translation-locale">
+        {{ translation.locale }}
+      </div>
+      <textarea>{{ translation.content }}</textarea>
+    </div>
+  </div>
+</template>
+
+<script>
+  import TranslationKey from "./translation-key.vue";
+
+  export default ({
+    name: 'translation-group',
+
+    props: {
+      translations: Array,
+      translationKey: String
+    },
+
+    components: {
+      translationKey: TranslationKey
+    }
+  });
+</script>
---
+++ src/frontend/app/start.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+import App from 'components/app.vue';
+import _ from 'lodash';
+import "whatwg-fetch";
+
+var store = {
+  groupedTranslations: []
+};
+
+window.vue = new Vue({
+  el: '#app',
+  data: store,
+  render(createElement) {
+    return createElement('app', {
+      props: {
+        store: this.$data
+      }
+    });
+  },
+  components: {
+    'app': App
+  }
+});
+
+fetch("/api/translations")
+  .then(response => response.json())
+  .then(data => {
+    store.groupedTranslations = _.groupBy(data, 'key');
+  });
