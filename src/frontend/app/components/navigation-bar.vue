67fa961aa66db7cc95386dfb27216e029cc0cb1f
Stanislas Signoud <signez@stanisoft.net>
2016-11-04 00:19:39 +0100

    Add a simple search bar

--- src/frontend/app/components/navigation-bar.vue
+++ src/frontend/app/components/navigation-bar.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="navigation-bar">
+    <div class="search-bar">
+      <input type="search" placeholder="Search for a key…" v-model="needle">
+      <i class="fa fa-fw fa-search" />
+    </div>
     <ul class="nav-translation-key-list">
-      <li v-for="key in translationKeys" class="nav-translation-key">
+      <li v-for="key in filteredTranslationsKeys" class="nav-translation-key">
         <a href="#" @click.prevent>
           <i class="fa fa-fw fa-file-o"></i>
           <span>{{ key }}</span>
@@ -13,11 +17,27 @@
 </template>
 
 <script>
+  import _ from "lodash";
+
   export default ({
     name: 'navigation-bar',
 
+    data() {
+      return {
+        needle: ""
+      };
+    },
+
     props: {
       translationKeys: Array
+    },
+
+    computed: {
+      filteredTranslationsKeys() {
+        return _.filter(this.translationKeys, (key) => {
+          return key.includes(this.$data.needle);
+        });
+      }
     }
   });
 </script>
