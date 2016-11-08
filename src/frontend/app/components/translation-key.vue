a1e997466cde1989ace32087d5a03b64413c8f1a
Stanislas Signoud <signez@stanisoft.net>
2016-11-08 20:09:20 +0100

    Add icons in crumbs for translation keys

--- src/frontend/app/components/translation-key.vue
+++ src/frontend/app/components/translation-key.vue
@@ -2,7 +2,12 @@
   <div class="translation-key">
     <!-- Spaceless to prevent unexpected spaces in copy-paste -->
     <span v-for="(crumb, index) in crumbs"
-      class="translation-crumb"><i class="fa fa-fw fa-caret-right translation-crumb-separator" v-if="index > 0"><span>.</span></i>{{ crumb }}</span>
+      class="translation-crumb"><!--
+    --><i class="fa fa-fw fa-caret-right translation-crumb-separator" v-if="index > 0"><span>.</span></i><!--
+    --><i class="fa fa-fw fa-folder" style="margin-right: 3px" v-if="index < crumbs.length - 1"></i><!--
+    --><i class="fa fa-fw fa-file-o" style="margin-right: 3px" v-if="index == crumbs.length - 1"></i><!--
+    -->{{ crumb }}<!--
+    --></span>
   </div>
 </template>
 
