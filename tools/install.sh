0094a15fa4c33b4d3873055a183bea571f5a5ed0
Raphaël Lustin <raphael@lustin.fr>
2016-11-19 19:06:38 +0100

    Move install in a dedicated script
    Also use rustup and force `diesel_cli` install.

---
+++ tools/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -ex
+
+command -v rustup >/dev/null 2>&1 || { echo 'I require `rustup` but it’s not installed. Install it with `curl https://sh.rustup.rs -sSf | sh`. Aborting.' >&2; exit 1; }
+
+sudo apt install libsqlite3-dev
+
+rustup override set nightly
+
+cargo install diesel_cli --force
+
+sudo npm install -g brunch
