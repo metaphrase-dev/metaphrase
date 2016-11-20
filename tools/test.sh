c837b5d24a4f48fd75e7b23074faecbe16feed44
Raphaël Lustin <raphael@lustin.fr>
2016-11-20 15:51:45 +0100

    Properly teardown tests on error
    It effectivly kills lugh server on error.

--- tools/test.sh
+++ tools/test.sh
@@ -1,5 +1,16 @@
 #!/bin/bash -ex
 
+function teardown {
+  kill -SIGTERM $LUGH_PID
+}
+
+function teardown_on_error {
+  teardown
+  exit 1
+}
+
+trap teardown_on_error ERR
+
 cargo build
 
 diesel setup
@@ -10,5 +21,5 @@ LUGH_PID=$!
 
 LUGH_BIND=127.0.0.1:3100 cargo test
 
-kill -SIGTERM $LUGH_PID
+teardown
 
