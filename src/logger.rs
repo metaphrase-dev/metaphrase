674c951934c724b6464609db6b0628a20582f516
Raphaël Lustin <raphael@lustin.fr>
2017-01-02 23:09:34 +0100

    Log error, elapsed time and http status code
    Also:
    - round time on milliseconds;
    - show 0 ms if time goes backwards.

--- src/logger.rs
+++ src/logger.rs
@@ -7,11 +7,29 @@ use time::precise_time_ns;
 
 pub struct RequestLogger;
 
+impl RequestLogger {
+    fn start(&self, request: &mut Request) {
+        request.extensions.insert::<RequestLogger>(precise_time_ns());
+    }
+
+    fn elapsed_time(&self, request: &mut Request) -> Result<u64, IronError> {
+        let start_time = *request.extensions.get::<RequestLogger>().unwrap();
+        let delta = (precise_time_ns() - start_time) as i64;
+
+        if delta <= 0 {
+            Ok(0)
+        } else {
+            Ok(((delta as f64) / 1000000.0).round() as u64)
+        }
+    }
+}
+
 impl typemap::Key for RequestLogger { type Value = u64; }
 
 impl BeforeMiddleware for RequestLogger {
     fn before(&self, request: &mut Request) -> IronResult<()> {
-        request.extensions.insert::<RequestLogger>(precise_time_ns());
+        self.start(request);
+
         println!(
             "Started {} \"{}\" for {} at {}",
             request.method,
@@ -26,9 +44,19 @@ impl BeforeMiddleware for RequestLogger {
 
 impl AfterMiddleware for RequestLogger {
     fn after(&self, request: &mut Request, response: Response) -> IronResult<Response> {
-        let delta = precise_time_ns() - *request.extensions.get::<RequestLogger>().unwrap();
-        println!("Request took {} ms", (delta as f64) / 1000000.0);
+        println!("{} ({} ms)", response.status.unwrap(), self.elapsed_time(request)?);
 
         Ok(response)
+    }
+
+    fn catch(&self, request: &mut Request, err: IronError) -> IronResult<Response> {
+        println!(
+            "{}: {} ({} ms)",
+            err.response.status.unwrap(),
+            err.error,
+            self.elapsed_time(request)?
+        );
+
+        Err(err)
     }
 }
