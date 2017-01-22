26dc6fba969101e2b5a32263d777300af1d92039
Nicolas Bouilleaud <nicolas.bouilleaud@capitainetrain.com>
2017-01-22 22:45:50 +0100

    Don’t check for $DATABASE_URL in install.sh
    It’ll never work: $DATABASE_URL is to be set in .env, which doesn’t exist at this point, and is not `source`d anyway.

--- tools/install.sh
+++ tools/install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -ex
 
-if [ -z $DATABASE_URL ]; then
-  echo "DATABASE_URL is not set. Aborting."
-  exit 1;
-fi
 
 command -v rustup >/dev/null 2>&1 || { echo 'I require `rustup` but it’s not installed. Install it with `curl https://sh.rustup.rs -sSf | sh`. Aborting.' >&2; exit 1; }
 
