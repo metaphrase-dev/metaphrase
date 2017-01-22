b3596ad2523bff7c0ca73d513727939ff95837cd
Nicolas Bouilleaud <nicolas.bouilleaud@capitainetrain.com>
2017-01-22 22:46:42 +0100

    Don’t try to install libsqlite on macOS
    (especially using apt)
    
    It’s already installed on any decent mac.

--- tools/install.sh
+++ tools/install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash -ex
 
+# libsqlite3-dev
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	sudo apt update
+	sudo apt install libsqlite3-dev
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	# macOS
+	# libsqlite3-dev is installed by default on any recent macOS
+else
+    echo "error can't install package `libsqlite3-dev`, unknown OS $OSTYPE"
+    exit 1;
+fi
 
 command -v rustup >/dev/null 2>&1 || { echo 'I require `rustup` but it’s not installed. Install it with `curl https://sh.rustup.rs -sSf | sh`. Aborting.' >&2; exit 1; }
-
-sudo apt update
-
-sudo apt install libsqlite3-dev
-
 rustup override set nightly
 
 cargo install diesel_cli --force
