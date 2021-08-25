b84f3159ac9ac68b166bacc82751e3f21aed8de9
Stanislas Signoud (Signez) <signez@stanisoft.net>
2021-08-25 06:05:05 +0200

    Ensure Vite understands routes with dots

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,12 +1,13 @@
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import pluginRewriteAll from "vite-plugin-rewrite-all";
 
 /**
  * https://vitejs.dev/config/
  * @type {import('vite').UserConfig}
  */
 export default {
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), pluginRewriteAll()],
   server: {
     port: 3100,
     proxy: {
